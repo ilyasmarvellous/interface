bdb3cabd79115a457aa4e1cd3379f4e2a16758e2
ilyas <ilyas@LAPTOP-NOQ3L7Q3>
2017-02-20 22:13:32 -0600

    Add project files.

---
+++ interface_ex/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    interface Icustomer1
+    {
+        void Print1();
+
+    }
+
+    interface Icustomer2 : Icustomer1
+    {
+        void Print2();
+
+    }
+    class Customer : Icustomer2
+    {
+        public void Print2()
+        {
+        Console.WriteLine("Print2");
+        }
+         public void Print1()
+        {
+        Console.WriteLine("Print1");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Customer c = new Customer();
+        c.Print1();
+        c.Print2();
+        Console.ReadKey();
+        }
+    }
+
